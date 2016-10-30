e64a06027501079b53ce173985fbbde4b81840f3
themukul <me.mukul05@gmail.com>
2016-10-30 16:29:30 +0530

    changes + about us

--- plinth/plinth/js/hamburger.js
+++ plinth/plinth/js/hamburger.js
@@ -1,3 +1,24 @@
+/*
 $('.ham-btn').click(function() {
     $('.ham-wrapper').toggleClass('fly-left');
 });
+*/
+
+$('.ham-btn').click(function() {
+    $('.ham-wrapper').animate({width: "250px"}, 200);
+});
+
+$('.ham-close').click(function() {
+    $('.ham-wrapper').animate({width: "0px"}, 200);
+});
+
+$('body').click(function() {
+    var toggle = $('.ham-wrapper').width();
+    if(toggle > 0){
+        $('.ham-wrapper').animate({width: "0px"}, 200);    
+    }
+});
+
+
+
+
