530bd12eeea99d8096200644bccb848d91c3c9cb
themukul <me.mukul05@gmail.com>
2016-11-07 03:02:13 +0530

    update

---
+++ plinth/plinth/js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    
+//    
+//    footerHandler = function(){
+//        var $el = $('.main-logo');
+//        var footerTop = $el.offset().top + $el.outerHeight(true);
+//    
+//        if( $(window).innerHeight() < 480){
+//            
+//        } 
+//        else{
+//            
+//        }
+//    }
+//    
+//    footerHandler();
+//    
+//    $(window).resize( function(){
+//        footerHandler();
+//    });
+    
+});
+
