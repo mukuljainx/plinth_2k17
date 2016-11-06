b9b0851fedb6f941b7ea2e554cfd4246056f938e
themukul <me.mukul05@gmail.com>
2016-11-06 15:23:05 +0530

    updates

--- plinth/plinth/js/hamburger.js
+++ plinth/plinth/js/hamburger.js
@@ -5,10 +5,12 @@ $('.ham-btn').click(function() {
 */
 
 $('.ham-btn').click(function() {
-    $('.ham-wrapper').animate({width: "250px"}, 200);
+    $('.ham-wrapper').animate({width: "200px"}, 200);
+    $('.ham-close').css("display", "block");
 });
 
 $('.ham-close').click(function() {
+    $('.ham-close').css("display","none");
     $('.ham-wrapper').animate({width: "0px"}, 200);
 });
 
@@ -16,6 +18,8 @@ $('body').click(function() {
     var toggle = $('.ham-wrapper').width();
     if(toggle > 0){
         $('.ham-wrapper').animate({width: "0px"}, 200);    
+        $('.ham-close').css("display","none");
+
     }
 });
 
