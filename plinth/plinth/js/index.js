b9f14ff62fcafa39c2dcd8b25d6bee9153b52979
Mukul Jain <jainmukul1996@gmail.com>
2016-11-07 03:42:45 +0530

    removed footer-fixed class from index.html fixed index.js

--- plinth/plinth/js/index.js
+++ plinth/plinth/js/index.js
@@ -4,9 +4,14 @@ $(document).ready(function() {
        var $el = $('.main-logo');
        var footerTop = $el.offset().top + $el.outerHeight(true);
 
-       if(footerTop + $('footer').height <  $(window).innerHeight() && $(window).innerWidth() > 640){
+       if(footerTop + $('footer').height() <  $(window).innerHeight() && $(window).innerWidth() > 640){
          $('footer').addClass('footer-fixed');
        }
+       else{
+         $('footer').removeClass('footer-fixed');
+       }
+       console.log("window hiehgt : " + $(window).innerHeight());
+       console.log("footer top : " + footerTop);
    }
 
    footerHandler();
