5632bf5c14cc3d1d9532c2a5f0485f7475d493f7
themukul <me.mukul05@gmail.com>
2016-11-29 04:44:17 +0530

    added all events updated reg form, reg form close js error not fixing all linked atv  not working, console error

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
-$(document).foundation()
+
+
+$(document).foundation();
 
 //HAMBURGER
 
--- public/js/reg-form.js
+++ public/js/reg-form.js
@@ -2,3 +2,16 @@ $('.reg-btn').click(function() {
     $('.reg-form').addClass('reg-form-display');
     $('.wrapper-form').addClass('reg-form-display');
 });
+
+
+    if($('.reg-form').hasClass('reg-form-display')){
+        $("[class != 'reg-form']").click(function() {
+            $('.reg-form').removeClass('reg-form-display');
+            $('.wrapper-form').removeClass('reg-form-display');
+        });    
+    }
+    
+
+
+
+
