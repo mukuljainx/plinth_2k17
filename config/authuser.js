85a7d1d88a26856897e155f6f280524c154b0879
Mukul Jain <jainmukul1996@gmail.com>
2016-12-13 04:08:19 +0530

    added one more user and href links to startup details

--- config/authuser.js
+++ config/authuser.js
@@ -1,4 +1,4 @@
 module.exports = {
     'ecell'   : ["mayuri.agrawal6547@gmail.com"],
-    'poc' : ["jainmukul1996@gmail.com","asaiatin@gmail.com","pvamsikrishna97@gmail.com","vbehl03@gmail.com","akshanshbindal.lnmiit@gmail.com","siddhant.000.singh@gmail.com","kapilmatani03@gmail.com","sethiasidharth@gmail.com","theshubham.dangi@gmail.com"],
+    'poc' : ["jainmukul1996@gmail.com","asaiatin@gmail.com","pvamsikrishna97@gmail.com","vbehl03@gmail.com","akshanshbindal.lnmiit@gmail.com","siddhant.000.singh@gmail.com","kapilmatani03@gmail.com","sethiasidharth@gmail.com","theshubham.dangi@gmail.com","romit.73@gmail.com"],
 };
