0b40a6219e71609a8d3e3a56c78212058092e26f
Mukul Jain <jainmukul1996@gmail.com>
2016-12-13 01:01:45 +0530

    removed env.process for auth user as it was not working with pm2 due to unknown reason

--- config/authuser.js
+++ config/authuser.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'poc'   : process.env.POC,
-    'ecell' : process.env.ECELL,
+    'ecell'   : ["mayuri.agrawal6547@gmail.com"],
+    'poc' : ["jainmukul1996@gmail.com","thakkarb97@gmail.com","asaiatin@gmail.com","pvamsikrishna97@gmail.com","vbehl03@gmail.com","akshanshbindal.lnmiit@gmail.com","siddhant.000.singh@gmail.com","kapilmatani03@gmail.com","sethiasidharth@gmail.com","theshubham.dangi@gmail.com"],
 };
