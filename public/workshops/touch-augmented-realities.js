e6f7b9720ea340623261be677c211e0f0998346c
Mukul Jain <jainmukul1996@gmail.com>
2016-12-26 14:55:38 +0530

    worshop 3

---
+++ public/workshops/touch-augmented-realities.js
@@ -0,0 +1,40 @@
+module.exports = {
+    eventName : "touch-augmented-realities",
+    displayName : "Touch and Augmented Realities",
+    eventDate : "7<sup>th</sup> - 8<sup>th</sup> Jan 2017",
+    eventCompany : "RoboFlux",
+    memberUpperLimit : 10,
+    memberLowerLimit : 1,
+    clubName : "workshop",
+    data : [
+        {
+            title : "Workshop Content",
+            content : ` <br><ol><li>Create Multi Touchpad</li>
+                        <li>Control PC with Hand Gestures and Eyes</li>
+                        <li>Convert laptop screen into touch</li>
+                        <li>Make Interactive Touch wall</li>
+                        <li>Control Music with Paper</li>
+                        <li>Make Kinect Xbox 360 applications</li>
+                        <li>Create a augmented world</li>
+                        <li>Gesture Interface </li>
+                        <li>Play games using motion sensing</li>
+                        <li>Create windows applications.</li></ol>`
+        },
+        {
+            title : "Benefits to participants",
+            content :  `<br><ol><li>National level certificate</li>
+                        <li>Tool kit CD & E-Books </li>
+                        <li>Hands-on- training</li>
+                        <li>Lifetime online support</li>
+                        <li>Attractive prizes</li>
+                        <li>internship offers</li>
+                        <li>20% discount on summer & winter training by Roboflux Technologies</li></ol>`
+        }
+    ],
+    eventDesc : `<p>Touch and Augmented Reality is the two days workshop to learn about sixth sense technologies on which Microsoft, Google, Intel & many more organisations currently working on it.</p>
+    <p>
+        For more know visit <a href="http://roboflux.com/robo_full_ads.php?pid=5" target="_blank">Roboflux</a>
+    </p>`,
+    imageLink : "media/icons/workshop/w3.svg",
+    fee : 750,
+};
--- routes/index.js
+++ routes/index.js
@@ -597,6 +597,33 @@ router.get('/workshops/vehicle-dynamics', Verify.verifyOrdinaryUser ,function(re
   }
 });
 
+router.get('/workshops/touch-augmented-realities', Verify.verifyOrdinaryUser ,function(req, res) {
+    var eventDetail = require('../public/workshops/touch-augmented-realities');
+
+  if(req.decoded.sub === ""){
+      isLoggedIn = false;
+      res.render('workshops/web-o-master', {
+          "isLoggedIn" : isLoggedIn,
+          eventDetail : eventDetail
+      });
+  }
+  else {
+      isLoggedIn = true;
+      User.findOne({'email' : req.decoded.sub }, function(err, user) {
+          // if there are any errors, return the error
+          if (err)
+              return done(err);
+          // check to see if theres already a user with that email
+          if (user){
+              res.render('workshops/web-o-master',{
+                  "isLoggedIn" : isLoggedIn,
+                  user : user,
+                  eventDetail : eventDetail
+              });
+          }
+      });
+  }
+});
 router.get('/profile', Verify.verifyOrdinaryUser ,function(req, res) {
 	if(req.decoded.sub === ""){
         isLoggedIn = false;
