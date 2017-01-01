9a4078b800ead195833d7b0f071e75f2e1e1f78e
Mukul Jain <jainmukul1996@gmail.com>
2017-01-02 00:05:03 +0530

    added basic function to display and hide leaderboard

---
+++ public/js/cryptex.js
@@ -0,0 +1,10 @@
+"use strict"
+function displayQuestion(){
+    $('.leaderboard').hide();
+    $('.question').show();
+}
+
+function displayLeaderBoard(){
+    $('.question').hide();
+    $('.leaderboard').show();
+}
