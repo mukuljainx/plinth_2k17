43d502f283a82db91cc72670cd4b3db254de20ad
Mukul Jain <jainmukul1996@gmail.com>
2017-01-03 03:26:17 +0530

    cryptex dashboard final

--- public/js/cryptex-dashboard.js
+++ public/js/cryptex-dashboard.js
@@ -1,1 +1,5 @@
-"use strict"
+Dropzone.options.myAwesomeDropzone = {
+  paramName: "file", // The name that will be used to transfer the file
+  maxFilesize: 0.5, // MB
+  acceptedFiles : 'image/*',
+};
--- routes/cryptex.js
+++ routes/cryptex.js
@@ -1,31 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var Eventx = require('../models/event');
 var User = require('../models/user');
-var UserEvent = require('../models/userevent');
+var Cryptex = require('../models/cryptex')
 var Verify = require('./verify');
-var Sif = require('../models/sif');
-var Robotics = require('../models/robotics');
-var Ecell = require('../models/ecell');
-var Quiz = require('../models/quiz');
-var Literary = require('../models/literary');
-var Astronomy = require('../models/astronomy');
-var Workshop = require('../models/workshop');
-var Cybros = require('../models/cybros');
 var authUser = require('../config/authuser');
-var PaymentDB = require('../models/payment');
-var PaymentMUN = require('../models/paymentMUN');
-var PaymentSIF = require('../models/paymentSIF');
-var EventURL = require('../config/eventURL');
-
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+var path = require('path');
+var fs = require('fs');
 
 router.get('/', Verify.verifyOrdinaryUser ,function(req, res) {
-  if(req.decoded.sub === ""){
-      isLoggedIn = false;
-      res.render('cryptex', {
-          "isLoggedIn" : isLoggedIn,
-      });
-  }
+
+    if(req.decoded.sub !== "jainmukul1996@gmail.com"){
+        isLoggedIn = false;
+        res.end('not authorized');
+    }
+  // if(req.decoded.sub === ""){
+  //     isLoggedIn = false;
+  //     res.render('cryptex', {
+  //         "isLoggedIn" : isLoggedIn,
+  //     });
+  // }
   else {
       isLoggedIn = true;
       User.findOne({'email' : req.decoded.sub }, function(err, user) {
@@ -47,11 +42,15 @@ router.get('/', Verify.verifyOrdinaryUser ,function(req, res) {
 });
 
 router.get('/play', Verify.verifyOrdinaryUser ,function(req, res) {
-  if(req.decoded.sub === ""){
-      isLoggedIn = false;
-      res.redirect('/cryptex');
-      return
-  }
+    if(req.decoded.sub !== "jainmukul1996@gmail.com"){
+        isLoggedIn = false;
+        res.end('not authorized');
+    }
+  // if(req.decoded.sub === ""){
+  //     isLoggedIn = false;
+  //     res.redirect('/cryptex');
+  //     return
+  // }
   else {
       isLoggedIn = true;
       User.findOne({'email' : req.decoded.sub }, function(err, user) {
@@ -60,10 +59,17 @@ router.get('/play', Verify.verifyOrdinaryUser ,function(req, res) {
               return done(err);
           // check to see if theres already a user with that email
           if (user){
-              res.render('cryptex_question',{
-                  isLoggedIn : isLoggedIn,
-                  user : user,
-              });
+              Cryptex.findOne({'level' : 1}, function(err,doc){
+                  if(err) throw err;
+                  else{
+                      console.log(doc);
+                      res.render('cryptex_question',{
+                          isLoggedIn : isLoggedIn,
+                          user : user,
+                          doc : doc,
+                      });
+                  }
+              })
           }
       });
   }
@@ -71,13 +77,18 @@ router.get('/play', Verify.verifyOrdinaryUser ,function(req, res) {
 
 
 router.get('/leaderboard', Verify.verifyOrdinaryUser ,function(req, res) {
-  if(req.decoded.sub === ""){
-      isLoggedIn = false;
-      res.render('cryptex_leaderboard', {
-          "isLoggedIn" : isLoggedIn,
-      });
-      return
-  }
+    if(req.decoded.sub !== "jainmukul1996@gmail.com"){
+        isLoggedIn = false;
+        res.end('not authorized');
+    }
+
+  // if(req.decoded.sub === ""){
+  //     isLoggedIn = false;
+  //     res.render('cryptex_leaderboard', {
+  //         "isLoggedIn" : isLoggedIn,
+  //     });
+  //     return
+  // }
   else {
       isLoggedIn = true;
       User.findOne({'email' : req.decoded.sub }, function(err, user) {
@@ -96,29 +107,87 @@ router.get('/leaderboard', Verify.verifyOrdinaryUser ,function(req, res) {
 });
 
 router.get('/dashboard', Verify.verifyOrdinaryUser ,function(req, res) {
-  if(req.decoded.sub === ""){
-      isLoggedIn = false;
-      res.render('cryptex_dashboard', {
-          "isLoggedIn" : isLoggedIn,
+    if(req.decoded.sub !== "jainmukul1996@gmail.com"){
+        isLoggedIn = false;
+        res.end('not authorized');
+    }
+    if(!isFinite(req.query.level)){
+        res.end('something wrong')
+        return;
+    }
+    if(req.decoded.sub === ""){
+        isLoggedIn = false;
+        res.render('cryptex_dashboard', {
+            "isLoggedIn" : isLoggedIn,
+        });
+        return
+    }
+    else {
+        isLoggedIn = true;
+        User.findOne({'email' : req.decoded.sub }, function(err, user) {
+        // if there are any errors, return the error
+        if (err)
+          return done(err);
+        // check to see if theres already a user with that email
+        if (user){
+            options = { upsert: true, new: true, setDefaultsOnInsert: true };
+            Cryptex.findOneAndUpdate({'level' : req.query.level},{$set : {'level' : req.query.level}}, options, function(err, doc){
+                if (err) throw err;
+                else{
+                    res.render('cryptex_dashboard',{
+                        isLoggedIn : isLoggedIn,
+                        user : user,
+                        levelNumber : req.query.level,
+                        doc : doc
+                    });
+                }
+            })
+      }
       });
-      return
-  }
-  else {
-      isLoggedIn = true;
-      User.findOne({'email' : req.decoded.sub }, function(err, user) {
-          // if there are any errors, return the error
-          if (err)
-              return done(err);
-          // check to see if theres already a user with that email
-          if (user){
-              res.render('cryptex_dashboard',{
-                  isLoggedIn : isLoggedIn,
-                  user : user,
-              });
-          }
-      });
-  }
+}
 });
 
+
+router.post('/editlevel/image/*', Verify.verifyOrdinaryUser, multipartMiddleware ,function(req, res) {
+    if(req.decoded.sub !== "jainmukul1996@gmail.com"){
+        isLoggedIn = false;
+        res.end('not authorized');
+    }
+    if(!isFinite(req.params['0'])){
+        res.end('something wrong')
+        return;
+    }
+    console.log(req.files.file);
+    var level = req.params['0'];
+    var tempPath = req.files.file.path;
+    var ext = path.extname(req.files.file.name);
+    relativePath = 'public/media/cryptex/'
+    targetPath = path.resolve(relativePath + level);
+    fs.rename(tempPath, targetPath, function(err) {
+        if (err) throw err;
+        Cryptex.findOneAndUpdate({'level' : req.params['0']}, {$set : {'image' : relativePath + level}},{new: true}, function(err,doc){
+            console.log("Upload completed!");
+        })
+    });
+    res.end('se');
+});
+
+router.post('/editlevel/question', Verify.verifyOrdinaryUser ,function(req, res) {
+    if(req.decoded.sub !== "jainmukul1996@gmail.com"){
+        isLoggedIn = false;
+        res.end('not authorized');
+    }
+    var update = {$set : {'question' : req.body.question, 'answer' : req.body.answer} };
+    var options = {new: true};
+    Cryptex.findOneAndUpdate({'level' : req.body.level}, update , options, function(err,doc){
+        if (err) throw err;
+        else {
+            console.log(doc);
+            console.log("Upload completed!");
+            res.json({response : true});
+            return;
+        }
+    })
+});
 
 module.exports = router;
