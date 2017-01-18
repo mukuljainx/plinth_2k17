f60f9c671e1dbfe99259155e6ef9c009438cd013
Mukul Jain <jainmukul1996@gmail.com>
2017-01-18 05:45:18 +0000

    commit from cloud, 500 -> 600, adui

--- public/workshops/audi.js
+++ public/workshops/audi.js
@@ -12,6 +12,6 @@ module.exports = {
 Auditya Venkatesh, better known as Audi, is a self taught photographer who is now a youth icon. He has over 8 lakhs followers on facebook and above 1.5 lakhs followers on Instagram. His top clientele includes brands like Vodafone, Samsung, Nissan and Toyota as his patrons. Also his interviews are included in economic times as well as India Today. He has also shot several videos and photos for Discovery Channel and has a youtube channel where he posts his work regularly, which also reaches the trending page of the country regularly. Auditya is also a motivational speaker and has given a Ted Talk.
     </p>`,
     imageLink : "media/icons/workshop/w4.svg",
-    fee : '500',
+    fee : '600',
     feeNote : '<span style="font-size:14px; color:#2199e8;">(Early Bird)</span>',
 };
--- routes/payment.js
+++ routes/payment.js
@@ -59,7 +59,7 @@ router.post('/fetchData', Verify.verifyOrdinaryUser, function(req, res) {
     if(req.body.eventName === "robowar") totalAmount = 700;
     if(req.body.eventName === "quadcopter") totalAmount = 600;
     if(req.body.eventName === "touch-augmented-realities") totalAmount = 750;
-    if(req.body.eventName === "audi") totalAmount = 500;
+    if(req.body.eventName === "audi") totalAmount = 600;
     if(req.body.eventName === "3dPrinting") totalAmount = 250;
 
 
@@ -112,7 +112,7 @@ router.get('/initiatepayment', function(req, res) {
     if(req.query.eventName === "robowar") totalAmount = 700;
     if(req.query.eventName === "quadcopter") totalAmount = 600;
     if(req.query.eventName === "touch-augmented-realities") totalAmount = 750;
-    if(req.body.eventName === "audi") totalAmount = 500;
+    if(req.body.eventName === "audi") totalAmount = 600;
     if(req.body.eventName === "3dPrinting") totalAmount = 250;
     payment = {
         status   : 'TXN_NOT_DONE',
@@ -135,7 +135,7 @@ router.get('/initiatepayment', function(req, res) {
                         var event_order_id = "Plinth-" + req.query.eventName + "-" + (count + 1) + "-" + id_tag;
 
                         if(req.query.eventName === "touch-augmented-realities") totalAmount = 750 * results.team.length;
-                        if(req.query.eventName === "audi") totalAmount = 500 * results.team.length; //workshop
+                        if(req.query.eventName === "audi") totalAmount = 600 * results.team.length; //workshop
                         if(req.query.eventName === "3dPrinting") totalAmount = 250 * results.team.length; //workshop
                         if(poc.indexOf(results.teamEmail) !== -1) totalAmount = 0.10;
                         paymentdb.id = id;
